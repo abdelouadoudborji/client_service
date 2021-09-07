2f0f798abde862ac0e6f9c3013fc9c3ac478a292
abdelouadoudborji <36171655+abdelouadoudborji@users.noreply.github.com>
2021-09-07 03:19:04 +0100

    Update Client.java

--- src/main/java/com/pantofit/client/entities/Client.java
+++ src/main/java/com/pantofit/client/entities/Client.java
@@ -1,7 +1,7 @@
 package com.pantofit.client.entities;
 import javax.persistence.*;
 
-import java.util.Date;
+import java.sql.Date;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.AllArgsConstructor;
