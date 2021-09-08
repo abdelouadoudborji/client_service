a564733dbbaa186fd0becdf963c1636290fe5d43
abdelouadoudborji <36171655+abdelouadoudborji@users.noreply.github.com>
2021-09-08 21:46:00 +0100

    Update ClientController.java

--- src/main/java/com/pantofit/client/web/ClientController.java
+++ src/main/java/com/pantofit/client/web/ClientController.java
@@ -5,7 +5,9 @@ import com.pantofit.client.dao.ClientRepository;
 import com.pantofit.client.entities.Client;
 import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
@@ -16,6 +18,21 @@ public class ClientController {
             public ClientController(ClientRepository clientRepository){
         this.clientRepository=clientRepository;
             }
+    
+    @GetMapping(path="/getClientCode/{code}")
+    public  Long getAbonnement(@PathVariable(name="code") String code){
+    Client client=this.clientRepository.findClientByCode(code);
+    if(client!=null){
+        System.out.println(clietn.getId());
+        return client.getId();
+    }
+    else{
+        return 0L;
+
+    }
+
+
+}
 
     @RequestMapping(value = "/getClient", method = RequestMethod.POST)
     @ResponseBody
